04a43699b6695450a70c0652d82262ce3ebd829f
CU-0xff <frank@cu-0xff.de>
2021-03-17 09:58:25 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var mysql = require('mysql');
+
+/* BAD FUNCTION - USES HARDCODED CREDENTIALS */
+var dbCon = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET LOG ENTRY FILE BASED ON FILE NAME */
+/* BAD FUNCTION - TRIGGERS TAINT ANALYSIS */
+router.get('/logFile', function(req, res) {
+  var logFileName = req.query.file || 'standard_log.log';
+
+  var logfile = fs.readFile(logFileName, "utf8", function(err, data) {
+    if (err) throw err;
+    res.render('logEntries', data);
+  });
+});
+
+
+/* GET LOG ENTRIES BASED ON SELECTED NODE */
+/* BAD FUNCTION - TRIGGERS TAINT ANALYSIS */
+router.get('/logEntries', function(req, res) {
+  var node = req.query.node || "Node0";
+
+  var db = req.db;
+
+  var collection = db.get(node);
+
+  collection.find({}).then(function(docs) {
+    res.render('logEntries', {"nodeName" : node, "log" : docs});
+  });
+});
+
+
+module.exports = router;
